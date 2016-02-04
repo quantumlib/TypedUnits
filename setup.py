94f4e4641b4344e0a525107d9421add2da254e28
Jimmy Chen <zchen@skynet>
2016-02-04 15:02:42 -0800

    Add np.get_include

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup, Extension
+import numpy as np
+
 setup(name="fastunits", version="1.0",
       packages = ['fastunits'],
+      include_dirs = [np.get_include()],
       ext_modules=[Extension("fastunits.unitarray", 
                              sources = ["fastunits/unitarray.c"])])
