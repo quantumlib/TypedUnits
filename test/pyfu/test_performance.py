b01c66b355aabe200e5a2f4ca424f144fa4009e1
Craig Gidney <Craig.Gidney@gmail.com>
2016-10-11 16:48:26 -0400

    Add str/repr perf test, tighten target for some multiplication tests
    Review: @DanielSank

--- test/pyfu/test_performance.py
+++ test/pyfu/test_performance.py
@@ -111,12 +111,12 @@ def test_perf_add(a, b):
     return a + b
 
 
-@perf_goal(avg_micros=10, args=[a_random_unit_val])
+@perf_goal(avg_micros=5, args=[a_random_unit_val])
 def test_perf_scale(a):
     return a * 3.14
 
 
-@perf_goal(avg_micros=10, args=[a_random_unit_val] * 2)
+@perf_goal(avg_micros=5, args=[a_random_unit_val] * 2)
 def test_perf_multiply(a, b):
     return a * b
 
@@ -126,7 +126,7 @@ def test_perf_get_item(a, b):
     return a[b]
 
 
-@perf_goal(avg_micros=10, args=[a_random_compatible_unit_val] * 2)
+@perf_goal(avg_micros=5, args=[a_random_compatible_unit_val] * 2)
 def test_perf_divmod(a, b):
     return divmod(a, b)
 
@@ -135,6 +135,17 @@ def test_perf_divmod(a, b):
 def test_perf_import_multiply_add_heterogeneous(a, b):
     from pyfu.units import kilometer, inch
     return a * kilometer + b * inch
+
+
+@perf_goal(avg_micros=20, args=[a_random_unit_val])
+def test_perf_str(a):
+    return str(a)
+
+
+# Note: another PR improves this 20X.
+@perf_goal(avg_micros=500, args=[a_random_unit_val])
+def test_perf_repr(a):
+    return repr(a)
 
 
 # ----- Array tests ------
