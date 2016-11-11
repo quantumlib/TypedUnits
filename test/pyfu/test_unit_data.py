003c5d6879513aa7e2220784b0f64a131476fe1a
Craig Gidney <Craig.Gidney@gmail.com>
2016-11-10 17:44:48 -0800

    Convert test_unit_data to using assert statements
    Review: @kunalq

--- test/pyfu/test_unit_data.py
+++ test/pyfu/test_unit_data.py
@@ -1,68 +1,63 @@
 import math
-import unittest
+
+import numpy as np
 
 
-class UnitDataTests(unittest.TestCase):
-    def testAllDefaultUnitsAndSimpleVariationsThereofAreParseable(self):
-        import pyfu.unit
-        db = pyfu.unit.default_unit_database
-        for k, u in db.known_units.items():
-            self.assertEqual(db.parse_unit_formula(k), u)
-            for v in [u, 1 / u, 5 * u, 1.1 * u, u**2]:
-                self.assertEqual(db.parse_unit_formula(str(v)), v)
+def test_all_default_units_and_simple_variations_thereof_are_parseable():
+    import pyfu.unit
+    db = pyfu.unit.default_unit_database
+    for k, u in db.known_units.items():
+        assert db.parse_unit_formula(k) == u
+        for v in [u, 1 / u, 5 * u, 1.1 * u, u**2]:
+            assert db.parse_unit_formula(str(v)) == v
 
-    def testEnergyStoredInCapacitor(self):
-        from pyfu.units import uF, V, uJ
-        capacitance = 2 * uF
-        voltage = 5 * V
-        stored = capacitance * voltage**2 / 2
-        self.assertEqual(stored, 25 * uJ)
+def test_unit_relationship_energy_stored_in_capacity():
+    from pyfu.units import uF, V, uJ
+    capacitance = 2 * uF
+    voltage = 5 * V
+    stored = capacitance * voltage**2 / 2
+    assert stored == 25 * uJ
 
-    def testDurations(self):
-        from pyfu.units import week, year, day, hour, minute, second
-        a = week + year + day + hour + minute
-        self.assertTrue(a.isCompatible(second))
-        self.assertEqual(round(year / week), 52)
-        self.assertAlmostEqual(year / second, 31557600)
+def test_durations():
+    from pyfu.units import week, year, day, hour, minute, second
+    a = week + year + day + hour + minute
+    assert a.isCompatible(second)
+    assert round(year / week) == 52
+    assert np.isclose(year / second, 31557600)
 
-    def testLengths(self):
-        from pyfu.units import inch, foot, yard, nautical_mile, angstrom, meter
-        a = inch + foot + yard + nautical_mile + angstrom
-        self.assertTrue(a.isCompatible(meter))
-        self.assertEqual((foot + inch + yard) * 5000, 6223 * meter)
-        self.assertAlmostEqual(nautical_mile / angstrom, 1.852e13)
+def test_lengths():
+    from pyfu.units import inch, foot, yard, nautical_mile, angstrom, meter
+    a = inch + foot + yard + nautical_mile + angstrom
+    assert a.isCompatible(meter)
+    assert (foot + inch + yard) * 5000 == 6223 * meter
+    assert np.isclose(nautical_mile / angstrom, 1.852e13)
 
-    def testAngles(self):
-        from pyfu.units import deg, rad, cyc
-        self.assertTrue((deg + cyc).isCompatible(rad))
-        self.assertAlmostEqual((math.pi * rad)[deg], 180)
-        self.assertAlmostEqual((math.pi * rad)[cyc], 0.5)
+def test_angles():
+    from pyfu.units import deg, rad, cyc
+    assert (deg + cyc).isCompatible(rad)
+    assert np.isclose((math.pi * rad)[deg], 180)
+    assert np.isclose((math.pi * rad)[cyc], 0.5)
 
-    def testVolumes(self):
-        from pyfu.units import (teaspoon,
-                                tablespoon,
-                                fluid_ounce,
-                                cup,
-                                pint,
-                                quart,
-                                us_gallon,
-                                british_gallon,
-                                liter)
-        a = (teaspoon + tablespoon + fluid_ounce + cup + pint + quart +
-             us_gallon + british_gallon)
-        self.assertTrue(a.isCompatible(liter))
-        self.assertAlmostEqual(british_gallon / us_gallon, 1.20095, places=6)
-        self.assertEqual(quart - pint - cup - tablespoon, 45 * teaspoon)
-        self.assertAlmostEqual(33.814 * fluid_ounce / liter, 1, places=5)
+def test_volumes():
+    from pyfu.units import (teaspoon,
+                            tablespoon,
+                            fluid_ounce,
+                            cup,
+                            pint,
+                            quart,
+                            us_gallon,
+                            british_gallon,
+                            liter)
+    a = (teaspoon + tablespoon + fluid_ounce + cup + pint + quart +
+         us_gallon + british_gallon)
+    assert a.isCompatible(liter)
+    assert np.isclose(british_gallon / us_gallon, 1.20095, atol=1e-6)
+    assert quart - pint - cup - tablespoon == 45 * teaspoon
+    assert np.isclose(33.814 * fluid_ounce / liter, 1, atol=1e-5)
 
-    def testMasses(self):
-        from pyfu.units import (ounce,
-                                pound,
-                                ton,
-                                megagram)
-        self.assertAlmostEqual(
-            (ounce + pound + ton) / megagram, 0.9077, places=4)
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_masses():
+    from pyfu.units import (ounce,
+                            pound,
+                            ton,
+                            megagram)
+    assert np.isclose((ounce + pound + ton) / megagram, 0.9077, atol=1e-4)
