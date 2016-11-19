97913bb075ad7f5c438c77aefc819f6bd85174e3
Craig Gidney <Craig.Gidney@gmail.com>
2016-11-18 22:35:46 -0500

    Add pytest_perf continuous integration script
    Review: -

---
+++ scripts/continuous_integration/jenkins/pytest_perf.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Checkout location.
+repo="repo"
+
+virtualenv .venv --clear --system-site-packages > script-noise.log
+. .venv/bin/activate >> script-noise.log
+pip install --upgrade pip >> script-noise.log
+pip install -r ${repo}/requirements.txt >> script-noise.log
+
+export PYTHONPATH=${repo}:${PYTHONPATH}
+
+# The report is consumed by the "Post-Build" XUnit action in Jenkins.
+cd ${repo}
+python setup.py build_ext --inplace
+pytest --junitxml report.xml test_perf/
