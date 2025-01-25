bea2930992500c3adef8e9f53900eb2335a01281
Nour Yosri <noureldinyosri@gmail.com>
2025-01-24 16:08:09 -0800

    lint

--- test/test_value_array.py
+++ test/test_value_array.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from packaging.version import Version
 import numpy as np
 import pytest
 from tunits.core import raw_WithUnit, raw_UnitArray
