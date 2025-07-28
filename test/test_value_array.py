8655f290d4031b957392bf863a9b55161cf60d90
Jonathan Gross <jarthurgross@google.com>
2025-07-28 16:16:38 -0700

    Add unique to stub file.

--- test/test_value_array.py
+++ test/test_value_array.py
@@ -292,4 +292,4 @@ def test_unique() -> None:
     xs = np.array([3.7, 0.9, 0.9, 4.3, 3.7])
     unit = tu.MHz
     v_arr = xs * unit
-    assert np.array_equal(v_arr.unique(), np.unique(xs) * unit) 
+    assert np.array_equal(v_arr.unique(), np.unique(xs) * unit)
